cea7f402512bbae926830b6c38aec7c65049bd29
Daniel Flídr <Daniel.Flidr@KornFerry.com>
2020-01-10 16:59:43 +0100

    troubleshooting

--- AutoTester.js
+++ AutoTester.js
@@ -30,7 +30,9 @@ class AutoTester{
     var questionIds : Object = {};
     for (var i = 0; i < testSubjects.dimensions.length; i++) {
       var dimension = testSubjects.dimensions[i];
+      this.log.LogDebug("2");
       if (dimension.Questions.isArray()){
+        this.log.LogDebug("3");
         for (var j = 0; j < dimension.Questions.length; j++) {
           if (questionIds[dimension.Questions[j]] === undefined){
             questionIds[dimension.Questions[j]] = true;
