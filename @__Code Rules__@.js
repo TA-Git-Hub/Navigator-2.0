0e779e3b1f8a0ce3b68df0a19e99d8e7ea6d6efb
Daniel Flídr <Daniel.Flidr@KornFerry.com>
2020-01-13 11:36:42 +0100

    basic instructions added to the Code Rules handbook

--- @__Code Rules__@.js
+++ @__Code Rules__@.js
@@ -1,1 +1,18 @@
-// to-do create rules for coding
+// Rules of Coding
+//CLASS
+// declaring a class -> name starts with a letter, first letter is uppercase, camelCase is used for the rest
+//            (all words are put together with first letter of each word being uppercase).
+// every class contains 5 parts:
+//            1) all property variables are declared;
+//            2) constructor(s);
+//            3) "CALCULATIONS" section, all functional functions are included there;
+//            4) getters and setters functions, getters are first, after last getter ends, first setter begins;
+//            5) other functions, particularly temporary functions (i.e. testing purposes).
+//VARIABLES
+// declaring a variable -> name starts with a letter, standard camelCase is used for the rest
+//            (all words are put together with first letter of each word being uppercase, except for first word).
+// variable data type is declared immediately (where possible).
+//FUNCTIONS
+// declaring a function -> name starts with a letter, first letter is uppercase, camelCase is used for the rest
+//            (all words are put together with first letter of each word being uppercase).
+// each function is declared with either "public" or "private" keyword.
