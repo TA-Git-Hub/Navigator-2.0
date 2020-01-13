b85c2c7ec97c0b0a9099e2308e0208d9c6e4ee8c
Lukáš Gryč <Lukas.Gryc@KornFerry.com>
2020-01-13 14:37:43 +0100

    Added whole class and both functions

---
+++ MainTable.js
@@ -0,0 +1,46 @@
+class MainTable{
+
+//Call this function from the aggregated table
+  static function GetTable(table, report){
+    var qs = MainTable.GetAllQuestions();
+
+    var X = [];
+    for(var i = 0; i < qs.length; i++){
+      X.push(qs[i] + '{title:true; totals:true}')
+    }
+    //There is no config for trend yet, so there is just this easy array of all years
+    var trendYears = [2016,2015,2014,2013]
+    var Y = [];
+
+    for(var j = 0; j < trendYears.length; j++){
+      Y.push('[SEGMENT]{' +
+              'label: "'+ trendYears[j] +'";' +
+              'hideheader:false;' +
+              'expression:' + report.TableUtils.EncodeJsString('Wave="' + trendYears[j] +'"')+
+              '}');
+    }
+
+    var x = X.join("+");
+    var y = Y.join("+");
+    var expr = [x, y].join('^');
+
+    table.AddHeaders(report, 'ds0', expr);
+}
+
+  static function GetAllQuestions(){
+    //Array with all GRIDs
+    var allGridObjects = Config.QuestionsGridStructure;
+    var allQuestions = [];
+
+    for(var i = 0; i < allGridObjects.length; i++){
+      if(allGridObjects[i].Qs != null){
+	    	for(var j = 0; j < allGridObjects[i].Qs.length; j++){
+          allQuestions.push(allGridObjects[i].Qs[j]);
+        }
+      }else{
+        allQuestions.push(allGridObjects[i].Id)
+      }
+    }
+    return allQuestions;
+  }
+}
