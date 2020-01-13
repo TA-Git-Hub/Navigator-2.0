0132a1b534dfaf272b33f4d8dea6be37281793fc
Daniel Flídr <Daniel.Flidr@KornFerry.com>
2020-01-13 11:42:45 +0100

    basic instruction added to the Code Rules handbook (edits)

--- @__Code Rules__@.js
+++ @__Code Rules__@.js
@@ -1,6 +1,6 @@
 // Rules of Coding
 //CLASS
-// declaring a class -> name starts with a letter, first letter is uppercase, camelCase is used for the rest
+// declaring a class -> name starts with an uppercase letter, camelCase is used for the rest
 //            (all words are put together with first letter of each word being uppercase).
 // every class contains 5 parts:
 //            1) all property variables are declared;
@@ -13,6 +13,6 @@
 //            (all words are put together with first letter of each word being uppercase, except for first word).
 // variable data type is declared immediately (where possible).
 //FUNCTIONS
-// declaring a function -> name starts with a letter, first letter is uppercase, camelCase is used for the rest
+// declaring a function -> name starts with an uppercase letter, camelCase is used for the rest
 //            (all words are put together with first letter of each word being uppercase).
 // each function is declared with either "public" or "private" keyword.
