715132a2c1a1cc893515860642fd6e31cb374c1f
Daniel Flídr <Daniel.Flidr@KornFerry.com>
2020-01-14 15:01:03 +0100

    Dany Filda

--- ReportQuestion.js
+++ ReportQuestion.js
@@ -1,11 +1,17 @@
 class ReportQuestion{
   private var id : String = null;
-  private var qName : String = null;
-  private var qText : String = null;
+  private var label : String = null;
+  private var description : String = null;
   private var distribution : int[] = []; // to-do
-  private var comparatorValues : int[] = []; // to-do must be sorted!!
-  private var scoreMapping = {Fav: null, Neu: null, Unfav: null};
-  private var validN : int = null;
+  private var comparatorValues = {trend: int[] = null,
+                        internal: int[] = null,
+                        external: int[] = null
+                      };
+  private var scores = {fav: int = null,
+                        neu: int = null,
+                        unfav: int = null,
+                        validN: int = null
+                      };
   private var flags = {}; // to-do SO, KDA, suppression... TRUE/FALSE
   private var apLink : String = null;
   private var orgcodes : String[] = []; // to-do determines local question visibility
@@ -16,8 +22,23 @@ class ReportQuestion{
   }
 
   // -- CALCULATIONS
-
-
+  public function Setup(information) {
+    if(information.label !== null){
+      SetLabel(information.label);
+    }
+    if(information.description !== null){
+      SetDecsription(information.description);
+    }
+    if(information.distribution !== null){
+      SetDistribution(information.distribution);
+    }
+    if(information.comparatorValues !== null){
+      SetComparatorValues(information.comparatorValues);
+    }
+    if(information.scores !== null){
+      SetScores(information.scores);
+    }
+  }
 
   // -- GETTERS
   public function GetId() {
@@ -25,17 +46,34 @@ class ReportQuestion{
   }
 
   // -- SETTERS
+  public function SetLabel(label) {
+      this.label = label;
+  }
+
+  public function SetDecsription(description) {
+    this.description = description;
+  }
+
+  public function SetDistribution(distribution) {
+    this.distribution = distribution;
+  }
+
+  public funtion SetComparatorValues(comparatorValues) {
+    this.comparatorValues.trend = comparatorValues.trend;
+    this.comparatorValues.internal = comparatorValues.internal;
+    this.comparatorValues.external = comparatorValues.external;
+
+  }
+
+  public function SetScores(scores) {
+    this.scores.fav = scores.fav;
+    this.scores.neu = scores.neu;
+    this.scores.unfav = scores.unfav;
+    this.scores.validN = scores.validN;
+  }
+
   public function SetFlags() {
 
   }
 
-  //TEST of Confirmit JS
-  public function SetOrgcodes(testArray : String[]) {
-    this.orgcodes = new String[testArray.length];
-
-    for(var i = 0; i < testArray.length; i++){
-      this.orgcodes[i] = testArray[i];
-      //this.orgcodes.push(testArray[i]);
-    }
-  }
 }
---
+++ TableHelper.js
@@ -0,0 +1,30 @@
+class TableHelper{
+  var allQIds = MainTable.GetAllGridQuestions();
+  private var reportQuestionHT = {};
+
+  static function GetMainTableData(context){
+    var columns = {text: context.report.TableUtils.GetColumnValues("source:MainTable", 0),
+                   currentResult: context.report.TableUtils.GetColumnValues("source:MainTable", 1)
+                   previous: GetPrevious(context)}
+    return columns;
+  }
+
+  static function GetPrevious(context){
+    var previous = Config.Wave.Previous;
+    var returnArray = [];
+    for(var i = 1; i < previous.length; i++){
+      returnArray[i-1] = context.report.TableUtils.GetColumnValues("source:MainTable", i+1);
+    }
+    return returnArray;
+  }
+
+  static function PopulateQuestions(context){
+    var columns = GetMainTableData(context);
+    for(var i = 0; i < allQIds.length; i++){
+      var question : ReportQuestion = new ReportQuestion(allQIds[i]);
+
+
+      question.Setup();
+    }
+  }
+}
