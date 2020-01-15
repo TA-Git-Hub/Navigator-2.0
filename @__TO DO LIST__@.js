4b98e141c1c373217a133eef816ddf450e51272f
Lukáš Gryč <Lukas.Gryc@KornFerry.com>
2020-01-15 09:59:27 +0100

    Added things to TODO

--- @__TO DO LIST__@.js
+++ @__TO DO LIST__@.js
@@ -6,11 +6,19 @@ Current:
 - class TableHelper:
       1) get distributions for questions
 
+Luky & Venca --
+- class MainTable:
+//How do we get the data for internal comparators? -> Filter by InHierarchy / Current Wave (just need a smart way to implement)
+//GetVerticalExpression - Filter Expression should have another function as this is probably the only thing that is not common
 */
 
 
 /*
 Finished:
 15.01.2020 - get numberOfAnswers from Config.GetDistributionIndexes(Id)
+           - class MainTable - added Horizontal & vertical expression function
+                             - Main table takes data and text from GRID, not individual questions
+                             - Added context object with all possible stuff we may need in the future
+           - class Config - Add wave, survey DataSources reference
 
 */
