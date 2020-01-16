3b7c4e7adbc3079f1bff846a7451a5ef26b733c7
Daniel Flídr <Daniel.Flidr@KornFerry.com>
2020-01-16 12:37:42 +0100

    troubleshooting

--- !TableHelper.js
+++ !TableHelper.js
@@ -26,8 +26,7 @@ static private function Debug(message, log){
 @@ context = report
 */
   static function PopulateQuestions(context){
-    //var columns = GetMainTableData(context);
-  //  Debug("1", context.log);
+    ReportHelper.Debug("1");
     var questionMap = CreateQuestionMap(context);
     var questionTexts = context.report.TableUtils.GetRowHeaderCategoryTitles("frodo:MainTable");
   //  Debug("2", context.log);
@@ -36,10 +35,12 @@ static private function Debug(message, log){
     var tempIt = 0;
     var columnCount = Config.Wave.Codes.length + 5; // 5 = number of internal comparators
     for(var i = 0; i < allQIds.length; i++){
+      ReportHelper.Debug("2A-" + i);
       var qValues = {current: null, trends: [], inter: [], exter: []};
     //  Debug("scale: " + questionMap[allQIds[i]], context.log);
       //Debug("i/length: " + i + '/' + allQIds.length, context.log);
       for(var columnIterator = 1; columnIterator <= columnCount; columnIterator++){
+        ReportHelper.Debug("2B-" + columnIterator);
         rowIterator = tempIt;
         //Debug("rowIterator: " + rowIterator, context.log);
 
@@ -51,14 +52,17 @@ static private function Debug(message, log){
         rowIterator += 1;
 
         if (columnIterator === 1) {
+          ReportHelper.Debug("3A");
           qValues.current = {distribution: distribution, validN: validN};
         }
 
         if (columnIterator > 1 && columnIterator <= Config.Wave.Codes.length) {
+          ReportHelper.Debug("3B");
           qValues.trends.push({distribution: distribution, validN: validN});
         }
 
         if (columnIterator > Config.Wave.Codes.length && columnIterator <= Config.Wave.Codes.length + 5){
+          ReportHelper.Debug("3C");
           qValues.inter.push({distribution: distribution, validN: validN});
         }
       }
@@ -67,7 +71,7 @@ static private function Debug(message, log){
       tempIt = rowIterator;
 
       var question : ReportQuestion = new ReportQuestion(allQIds[i]);
-      question.Setup({distribution: qValues.current.distribution, validN : qValues.current.validN, label: label, comparatorValues: {trend: qValues.trends}, description: ""}, context);
+      question.Setup({distribution: qValues.current.distribution, validN : qValues.current.validN, label: label, comparatorValues: {trend: qValues.trends, inter: qValues.inter}, description: ""}, context);
       returnArray.push(question);
     }
     return returnArray;
