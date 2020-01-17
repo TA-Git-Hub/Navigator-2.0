16a4df610f99b092ab5d09412869bc0505ccd918
Filip Muchka <Filip.Muchka@KornFerry.com>
2020-01-17 15:18:54 +0100

    JSON helper

---
+++ !JSONHelper.js
@@ -0,0 +1,38 @@
+class JSONHelper{
+  public static function GetJSON(obj){
+
+        var t = typeof (obj);
+        if (t !== "object" || obj === null) {
+            // simple data type
+            if (t === "string") obj = '"' + EscapeEntities(obj) + '"';
+            else if(t === "number") obj = '"' + obj + '"';
+
+            return String(obj);
+        }
+        else {
+            // recurse array or object
+            var n, v, json = [], arr = (obj && obj.constructor === Array);
+            for (n in obj) {
+                v = obj[n]; t = typeof(v);
+                if (t === "string"){
+                    v = '"' + EscapeEntities(v) + '"';
+                }
+                else if (t === "object" && v !== null) v = GetJSON(v);
+                json.push((arr ? "" : '"' + n + '":') + String(v));
+            }
+            return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+        }
+  }
+
+  private static function EscapeEntities(str) {
+    var entitiesMap = {
+        '<': '&lt;',
+        '>': '&gt;',
+        '&': '&amp;',
+        '\"': '\\&quot;'/*,
+    };
+    return str.replace(/[&<>\"]/g, function(key) {
+        return entitiesMap[key];
+    });
+  }
+}
