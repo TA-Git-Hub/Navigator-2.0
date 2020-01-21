8301eb55bcb310eee54c6a0f1707a62376adab25
Daniel Flídr <Daniel.Flidr@KornFerry.com>
2020-01-21 15:06:15 +0100

    blah blah blah

--- !ReportHelper.js
+++ !ReportHelper.js
@@ -22,12 +22,12 @@ class ReportHelper{
     var replacement = null;
     var returnString = text;
     if(text.indexOf(textReplace.placeholder1) !== -1){
-      replacement = context.report.DataSource.GetProject(Config.DataSources.MainSurvey).GetQuestion(textReplace.questionId).GetAnswer(textReplace.replacement1);
+      replacement = context.report.DataSource.GetProject(Config.DataSources.MainSurvey).GetQuestion(textReplace.questionId).GetAnswer(textReplace.replacement1).Text;
       returnString = text.split(textReplace.placeholder1).join(replacement);
     }
 
     if(text.indexOf(textReplace.placeholder2) !== -1){
-      replacement = context.report.DataSource.GetProject(Config.DataSources.MainSurvey).GetQuestion(textReplace.questionId).GetAnswer(textReplace.replacement2);
+      replacement = context.report.DataSource.GetProject(Config.DataSources.MainSurvey).GetQuestion(textReplace.questionId).GetAnswer(textReplace.replacement2).Text;
       returnString = text.split(textReplace.placeholder2).join(replacement);
     }
 
