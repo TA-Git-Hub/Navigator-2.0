4f3383ecf2e3c8ea2fc23f046439f9ba048d3f13
Erik Šiffel <Erik.Siffel@KornFerry.com>
2020-01-21 16:38:14 +0100

    dim 1

--- ReportDimension.js
+++ ReportDimension.js
@@ -1,69 +1,37 @@
-/*class ReportDimension {
+class ReportDimension {
 
-  //Array for question objects
-  private var questionArray = {};
-
-  //ID and texts
-  private  var id: String = null;
-  private  var label: String = null;
-  private  var description: String = null;
-
-  //Statistics from table
-  private var scores = {fav: int = null,
-                        neu: int = null,
-                        unfav: int = null,
-                        validN: int = null
-                      }
-
- //todo Question class includes count, valid N , fav
-
-  private var comparatorValues = {};
+  private var questionArray = {}; //Array for question objects
+  private var id: String = null;
+  private var label: String = null;
+  private var description: String = null;
 
   private var flags = {}; // to-do SO, KDA, suppression... TRUE/FALSE
   private var apLink: String = null;
   private var orgcodes: String[] = []; // to-do determines local question visibility
 
+  //Dimension Details
+  private var fav: int = null;
+  private var neu: int = null;
+  private var unfav: int = null;
+  private var validN: int = null;
+  private var comparatorValues = {};
+//----------------------------------------------
   // constructor
-  public function ReportDimension(id : String) {
-    this.id = id;
-    questionArray = getQuestions(id);
-
-    for (var stat in scores){
-      if checkSuppression(){
-        scores.stat=getScore(stat);
-      }
-      else{
-        scores.stat=-1;
-      }
+  public function ReportDimension(dim) {
+    this.id = dim.id;
+    this.questionArray = loadQuestionsToDimension(dim);
+    this.description = getDimDescription()
+  }
+//----------------------------------------------
+  function getDimDescription(){
+  };
+//----------------------------------------------
+  function loadQuestionsToDimension(dim){
+    for (var j = 0; j < dim.questions.length; j++) {
+      log.LogDebug('a');
+    var qID = dim.questions[j];
+      log.LogDebug(qID);
+    questionArray[qID]= allQ[qID];
     }
   }
-
-  public function checkSuppression(){
-    return false;
-  }
-
-//Get Dimension score calculated from question scores
-  public function getScore (stat){
-    var count = 0;
-      var sum = 0;
-      for (var i = 0; i < questionArray.length; i++) {
-        var q = questionArray[i];
-        sum += q.stat;
-        count ++;
-      }
-      return = Math.round(sum/count);
-  }
-
-
-//add question objects into dimension object
-  private function getQuestions (dID){
-    for (var i = 0; i < Config.Dimensions[dID].length; i++) {
-      var q = new ReportQuestion(Config.Dimensions[dID][i]);
-      questionArray.qID=q;
-    }
-  }
-
-
-
-}
-*/
+//----------------------------------------------}
