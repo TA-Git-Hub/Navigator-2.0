998d9153d3150c6c9d8065e175d142ce8edb4ddf
Erik Šiffel <Erik.Siffel@KornFerry.com>
2020-01-27 19:46:49 +0100

    ew

--- ReportDimension.js
+++ ReportDimension.js
@@ -1,94 +1,80 @@
-class ReportDimension{
+class ReportDimension {
 
-  public var questionArray = {}; //Array for question objects
-//----------------------------------------------
-  public var id = "";
-  public var label = "";
-  public var description = "";
-  public var flags = {}; // to-do SO, KDA, suppression... TRUE/FALSE
-  public var apLink = "";
-
-  //Dimension Results
-  public var results = {};
-
+  var questionArray = {}; //Array for question objects
+  var id = "";
+  var label = "";
+  var description = "";
+  var flags = {}; // to-do SO, KDA, suppression... TRUE/FALSE
+  var apLink = "";
+  var results = {};
 
   // constructor---------------------------------
-  public function ReportDimension(dim, allQ, context) {
+  function ReportDimension(dim, allQ, context) {
     this.id = dim.Id;
     this.label = this.getDimLabel();
     this.description = getDimDescription();
-    this.loadQuestionsToDimension(dim, allQ, context);
-    this.calculateDimResults(context);
-   }
+    loadQuestionsToDimension(dim, allQ, context);
+    calculateDimResults(context);
+  }
 
   //fill dimension-----------------------------
-    function loadQuestionsToDimension(dim, allQ, context){
-      for (var i = 0; i < dim.Questions.length; i++) {
-        var qID = dim.Questions[i];
-        this.questionArray[qID]= allQ[qID];
-      }
+  function loadQuestionsToDimension(dim, allQ, context) {
+    for (var i = 0; i < dim.Questions.length; i++) {
+      var qID = dim.Questions[i];
+      this.questionArray[qID] = allQ[qID];
     }
-
-    function calculateDimResults(context){
-      for (var i = 0; i<Config.Wave.Codes.length; i++){
-      var compID = ConfigHelper.waveID(i);
-      context.log.LogDebug(11);
-      this.getScores(compID, context);
-      }
-      for (var i = 0; i<Config.Internal; i++){
-      context.log.LogDebug(12);
-      var compID = 'internal'+(i);
-      this.getScores(compID, context);
-      }
-    }
-
-    public function GetJSONString(context){
-    return {id: this.id, label: this.label, description:this.description, results: this.results, questionArray:this.questionArray, flags: this.flags, apLink: this.apLink};
-    }
-
-  //public function convertComp
-
-public function getScores(compID, context){
-  //context.log.LogDebug(111);
-  this.results[compID]={}
-  var resultsType=['fav','neu','unfav','validN'];
-    //context.log.LogDebug(1111);
-  for (var j=0; j<resultsType.length;j++){
-  //  context.log.LogDebug(13);
-  var type = resultsType[j];
-  this.results[compID][type] = setDimScore(type, compID, context);
-    }
-  this.results[compID]['comp'] = {};
-
-}
-
-    function setDimScore(score, compID, context){
-      var total = 0;
-      var count = 0;
-          for (var q in this.questionArray){
-                //context.log.LogDebug(14);
-                //  context.log.LogDebug(score + compID +q);
-          total = total +  questionArray[q].details[compID][score];
-        count = count + 1;
-        }
-    //context.log.LogDebug(Math.round(total/count) + ' ' + total/count)
-    return Math.round(total/count);
   }
 
-    function setDimValidN() {
-      return 200;
-
+  //----------------Fill dimension scores
+  function calculateDimResults(context) {
+    for (var i = 0; i < Config.Wave.Codes.length; i++) {
+      var compID = ConfigHelper.waveID(i);
+      this.getScores(compID, context);
     }
+    for (var i = 0; i < Config.Internal; i++) {
+      var compID = 'internal' + (i);
+      this.getScores(compID, context);
+    }
+  }
 
-//----------------------------------------------
-  function getDimDescription(){
-  return Config.Wave.Codes[0];
-  };
+  function getScores(compID, context) {
+    this.results[compID] = {}
+    var resultsType = ['fav', 'neu', 'unfav', 'validN'];
+    for (var j = 0; j < resultsType.length; j++) {
+      var type = resultsType[j];
+      this.results[compID][type] = setDimScore(type, compID, context);
+    }
+    this.results[compID]['comp'] = {};
+  }
 
-  function getDimLabel(){
-  return Config.Wave.Codes[0];
-
-  };
+  function setDimScore(score, compID, context) {
+    var total = 0;
+    var count = 0;
+    for (var q in this.questionArray) {
+      total = total + questionArray[q].details[compID][score];
+      count = count + 1;
+    }
+    return Math.round(total / count);
+  }
 
   //----------------------------------------------
+  function getDimDescription() {
+    return Config.Wave.Codes[0];
+  };
+
+  function getDimLabel() {
+    return Config.Wave.Codes[0];
+  };
+  //----------------------------------------------
+  function GetJSONString(context) {
+    return {
+      id: this.id,
+      label: this.label,
+      description: this.description,
+      results: this.results,
+      questionArray: this.questionArray,
+      flags: this.flags,
+      apLink: this.apLink
+    };
   }
+}
