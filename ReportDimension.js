0487f4cffe157fbd7f52ad789d5d6d23b62e7bae
Erik Šiffel <Erik.Siffel@KornFerry.com>
2020-01-27 17:52:10 +0100

    f

--- ReportDimension.js
+++ ReportDimension.js
@@ -63,7 +63,7 @@ class ReportDimension{
       var count = 0;
       context.log.LogDebug(waveID);
       for (var q in this.questionArray){
-        context.log.LogDebug('score');
+        context.log.LogDebug(score);
         context.log.LogDebug(q.details[waveID][score]);
       total = total +  q.details[waveID][score];
         context.log.LogDebug('18');
