c0fa90e18dfb7094ffde468071e659403b22f413
Daniel Flídr <Daniel.Flidr@KornFerry.com>
2020-01-28 14:29:47 +0100

    asda

--- ReportDetails.js
+++ ReportDetails.js
@@ -45,8 +45,10 @@ class ReportDetails{
     this.unfav = 0;
 
     for(var i = 0; i < this.scale.length; i++){
+
       switch(this.scale[i].Weight){
         case 1 :
+        ReportHelper.Debug('Distribution: ' + this.distribution[i]);
           this.fav += this.distribution[i];
           break;
         case 0 :
@@ -66,16 +68,16 @@ class ReportDetails{
 
     switch (type) {
       case 'count/validN':
-      ReportHelper.Debug('favBefore: ' + this.fav);
+    //  ReportHelper.Debug('favBefore: ' + this.fav);
         this.fav = (this.validN === 0) ? -1 : Math.round((this.fav / this.validN)*100);
 
-        ReportHelper.Debug('favAfter: ' + this.fav);
+      //  ReportHelper.Debug('favAfter: ' + this.fav);
 
-        ReportHelper.Debug('neuBefore: ' + this.neu);
-        var temp = this.neu;
+      /*  ReportHelper.Debug('neuBefore: ' + this.neu);
+        var temp = this.neu;*/
         this.neu = (this.validN === 0) ? -1 : Math.round((temp / this.validN)*100);
 
-        ReportHelper.Debug('neuafter: ' + this.neu);
+      //  ReportHelper.Debug('neuafter: ' + this.neu);
         this.unfav = (this.validN === 0) ? -1 : Math.round((this.unfav / this.validN)*100);
         break;
       default:
